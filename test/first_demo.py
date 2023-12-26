d81fa8c1a31baa8d36d42c6790d7ae9e1c1c1e91
ruixCMU <ruiwang3@andrew.cmu.edu>
2023-12-25 23:36:29 -0500

    wrote a first demo

---
+++ test/first_demo.py
@@ -0,0 +1,6 @@
+import npai.machine_learning as npml
+import npai.ensemble as npen
+import npai.deep_learning as npdl
+import npai.neural_network as npnn
+import npai.optimization as npop
+import npai.reinforcement_learning as nprl
