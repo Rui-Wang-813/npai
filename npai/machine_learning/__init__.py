4475ebf078c52aac36d93bddcbcb0087d2e02f68
Erikellerx <eric.huahua1999@gmail.com>
2023-12-29 15:07:16 +0800

    added PCA

--- npai/machine_learning/__init__.py
+++ npai/machine_learning/__init__.py
@@ -1,7 +1,9 @@
 from .base import *
 from .linear_models import LinearRegression, RidgeRegression, LassoRegression
 from .logistic_models import LogisticRegression, MulticlassLogisticRegression
+from .pca import PCA
 
 __all__ = [
-    "LinearRegression", "RidgeRegression", "LassoRegression", "LogisticRegression", "MulticlassLogisticRegression"
+    "LinearRegression", "RidgeRegression", "LassoRegression", "LogisticRegression", "MulticlassLogisticRegression",
+    "PCA"
 ]
---
+++ npai/machine_learning/pca.py
@@ -0,0 +1,35 @@
+from .base import *
+import numpy as np
+
+
+class PCA(Estimator):
+    """
+    Principal Component Analysis (PCA)
+    """
+    def __init__(self, n_components=None):
+        """
+        :param n_components: int, the number of components to keep
+        """
+        self.n_components = n_components
+        self.components = None
+        self.explained_variance = None
+
+    def fit(self, X):
+        """
+        Fit the model with X.
+        :param X: array-like of shape (n_samples, n_features)
+        :param y: array-like of shape (n_samples,) or (n_samples, n_targets)
+        :param verbose: bool, whether to print the loss
+        :return: self
+        """
+        X = X - np.mean(X, axis=0)
+
+        covMat = np.divide(np.matmul(X.T, X), X.shape[0] )
+        eigenvalues, eigenvectors = np.linalg.eig(covMat)
+        idx = eigenvalues.argsort()[::-1]
+        eigenvalues = eigenvalues[idx]
+        eigenvectors = eigenvectors[:,idx]
+        self.components = eigenvectors[:, :self.n_components]
+
+        return self
+    
